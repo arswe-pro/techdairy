dd0cde7a9ab1d198a0610f9dfded2915cf28e526
Abdur rahman <abdur.rahman200301@gmail.com>
2021-06-27 13:04:38 +0600

    add account

--- src/Screen/Detail.jsx
+++ src/Screen/Detail.jsx
@@ -13,9 +13,9 @@ const Detail = ({ match }) => {
         <>
             <Button type='dashed' size='large' shape='round' style={{ margin: '2rem', color: '#FFF' }}> <Link to='/'> Go Back</Link> </Button>
             <Row justify='center' align='middle'>
-                <Col xs={23} sm={22} md={16} lg={16} xl={16} xxl={16} style={{ margin: "2rem 2rem" }}>
-                    <Card hoverable cover={<img alt={post.name} src={post.image} />} style={{ height: "100%" }} >
-                        <Meta title={post.name} description={post.description} />
+                <Col xs={23} sm={22} md={14} lg={14} xl={14} xxl={14} style={{ margin: "2rem 2rem" }}>
+                    <Card title={post.name} hoverable cover={<img alt={post.name} src={post.image} />} style={{ height: "100%" }} >
+                        <Meta title={post.Brand} description={post.description} />
                     </Card>
                 </Col>
             </Row>
